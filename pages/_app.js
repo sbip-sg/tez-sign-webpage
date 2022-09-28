79ae1f4a32848c33f5a7e090d048da3947abf975
Jon T <41064987+jonathantayyw@users.noreply.github.com>
2022-09-28 16:48:10 +0800

    convert to public

--- pages/_app.js
+++ pages/_app.js
@@ -50,7 +50,7 @@ export default function TezSign(props) {
   const { Component, pageProps } = props;
 
   React.useEffect(() => {
-    // Remove the server-side injected CSS.
+    /* Remove the server-side injected CSS. */
     const jssStyles = document.querySelector("#jss-server-side");
     if (jssStyles) {
       jssStyles.parentElement.removeChild(jssStyles);
